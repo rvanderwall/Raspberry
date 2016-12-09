052d00eb2e3f7d1baf7c6860c7c61757cc01f6a0
Robert Vanderwall <rvanderwall@yahoo.com>
2016-12-08 21:29:43 -0500

    fix Hand

--- src/Servo1/hand.py
+++ src/Servo1/hand.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python
 
 from Servo import Servo
-from Motion import Position
-import time
-
+from Position import Position
 
 servo0 = Servo(0)
 servo1 = Servo(1)
 p = Position()
 
 while True:
-        (x_pos, y_pos, zpos) = p.get_position()
+        (x_pos, y_pos, z_pos) = p.get_position()
+        print("X:{} Y:{} Z:{}".format(x_pos, y_pos, z_pos))
 
         x_pos_target  = int((1.0 + x_pos) * 50)
         y_pos_target  = int((1.0 + y_pos) * 50)
